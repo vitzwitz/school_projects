1bce17b0d2d52e1d0d3ad0cbd9ed218d863f7246
vitzwitz <blm7643@rit.edu>
2017-09-05 21:46:48 -0400

    Add files via upload

---
+++ myNode.py
@@ -0,0 +1,12 @@
+"""
+Nodes
+file: myNode.py
+author: Sean Strout
+author: Arthur Nunes-Harwitt
+"""
+
+from rit_lib import *
+
+
+class Node(struct):
+    _slots = ( (object, 'data'), ((NoneType, 'Node'), 'next') )
